a16bdce7824440162c7c58e063e231a2aaff2752
Robert Niederreiter <rnix@squarewave.at>
2012-02-14 21:38:02 +0100

    schema interface generation basically works

--- src/agx/generator/dexterity/dxgenerator.py
+++ src/agx/generator/dexterity/dxgenerator.py
@@ -1,36 +1,157 @@
+from node.ext.uml.utils import (
+    TaggedValues,
+    UNSET,
+)
 from agx.core import handler
+from agx.core.util import read_target_node
+from agx.generator.dexterity.schema import mapping
+
+
+def read_tgv(tgv, attribute, stereotype, attrs):
+    for attr in attrs:
+        value = tgv.direct(attr, stereotype)
+        if value is not UNSET:
+            attribute.kwargs[attr] = value
+
+
+def field_tgv(tgv, attribute, stereotype):
+    """
+    - title: String
+    - description: String
+    - required: Bool
+    - readonly: Bool
+    - default: String
+    - missing_value: String
+    """
+    attrs = ['title', 'description', 'required', 'readonly', 'default',
+             'missing_value']
+    read_tgv(tgv, attribute, stereotype, attrs)
+
+
+def minmaxlen_tgv(tgv, attribute, stereotype):
+    """
+    - min_length: Int
+    - max_length: Int
+    """
+    field_tgv(tgv, attribute, stereotype)
+    attrs = ['min_length', 'max_length']
+    read_tgv(tgv, attribute, stereotype, attrs)
+
+
+def collection_tgv(tgv, attribute, stereotype):
+    """
+    - value_type: String
+    - unique: Bool
+    """
+    minmaxlen_tgv(tgv, attribute, stereotype)
+    attrs = ['value_type', 'unique']
+    read_tgv(tgv, attribute, stereotype, attrs)
+
+
+def dict_tgv(tgv, attribute, stereotype):
+    """
+    - key_type: String
+    - value_type: String
+    """
+    minmaxlen_tgv(tgv, attribute, stereotype)
+    attrs = ['key_type', 'value_type']
+    read_tgv(tgv, attribute, stereotype, attrs)
+
+
+def richtext_tgv(tgv, attribute, stereotype):
+    """
+    - default_mime_type: String
+    - output_mime_type: String
+    - allowed_mime_types: String
+    """
+    field_tgv(tgv, attribute, stereotype)
+    attrs = ['default_mime_type', 'output_mime_type', 'allowed_mime_types']
+    read_tgv(tgv, attribute, stereotype, attrs)
+
+
+def minmax_tgv(tgv, attribute, stereotype):
+    """
+    - min: String
+    - max: String
+    """
+    field_tgv(tgv, attribute, stereotype)
+    attrs = ['min', 'max']
+    read_tgv(tgv, attribute, stereotype, attrs)
+
+
+def object_tgv(tgv, attribute, stereotype):
+    """
+    - schema: String
+    """
+    field_tgv(tgv, attribute, stereotype)
+    attrs = ['schema']
+    read_tgv(tgv, attribute, stereotype, attrs)
+
+
+def field_def_for(group, stereotype):
+    field = mapping[group].get(stereotype)
+    if not field:
+        return None
+    return {
+        'factory': field['factory'],
+        'import': field.get('import', 'schema'),
+        'import_from': field.get('import_from', 'zope'),
+        'depends':  field.get('depends'),
+        'stereotype': stereotype,
+    }
+
+
+def lookup_field_def(source, group):
+    field = None
+    for st in source.stereotypes:
+        field = field_def_for(group, st.name)
+        if field:
+            break
+    if not field:
+        raise RuntimeError(u"Field definition not found for %s" % source.name)
+    return field
+
+
+def transform_attribute(source, target, group, fetch_tgv):
+    field_def = lookup_field_def(source, group)
+    attribute = read_target_node(source, target.target)
+    attribute.value = field_def['factory']
+    tgv = TaggedValues(source)
+    fetch_tgv(tgv, attribute, field_def['stereotype'])
+    # XXX not works
+    #attribute.postlf = 5
 
 
 @handler('dxcollection', 'uml2fs', 'zcagenerator', 'dxcollection', order=100)
 def dxcollection(self, source, target):
-    print source
+    transform_attribute(source, target, 'collection', collection_tgv)
 
 
 @handler('dxminmaxlen', 'uml2fs', 'zcagenerator', 'dxminmaxlen', order=100)
 def dxminmaxlen(self, source, target):
-    print source
+    transform_attribute(source, target, 'minmaxlen', minmaxlen_tgv)
 
 
 @handler('dxdict', 'uml2fs', 'zcagenerator', 'dxdict', order=100)
 def dxdict(self, source, target):
-    print source
+    transform_attribute(source, target, 'dict', dict_tgv)
 
 
 @handler('dxfield', 'uml2fs', 'zcagenerator', 'dxfield', order=100)
 def dxfield(self, source, target):
-    print source
+    transform_attribute(source, target, 'field', field_tgv)
 
 
 @handler('dxrichtext', 'uml2fs', 'zcagenerator', 'dxrichtext', order=100)
 def dxrichtext(self, source, target):
-    print source
+    transform_attribute(source, target, 'richtext', richtext_tgv)
 
 
 @handler('dxminmax', 'uml2fs', 'zcagenerator', 'dxminmax', order=100)
 def dxminmax(self, source, target):
-    print source
+    transform_attribute(source, target, 'minmax', minmax_tgv)
 
 
 @handler('dxobject', 'uml2fs', 'zcagenerator', 'dxobject', order=100)
 def dxobject(self, source, target):
-    print source
+    transform_attribute(source, target, 'object', object_tgv)
